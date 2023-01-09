3f23895273e2f578d47a6c4c7544b5ce40414858
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2023-01-09 21:08:25 +0530

    Another Method

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-(function() {
-
-    let screen = document.querySelector('.screen');
-    let buttons = document.querySelectorAll('.btn');
-    let clear = document.querySelector('.btn-clear');
-    let equal = document.querySelector('.btn-equal');
-
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function(e) {
-                let value = e.target.dataset.num;
-                screen.value += value;
-            })
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let equal = document.querySelector(".btn-equal");
+  let clear = document.querySelector(".btn-clear");
+  //Method 2
+  //   let equal = document.querySelector("#equal");
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      //Method 1
+      if (e.target.dataset.num != "=") {
+        let value = e.target.dataset.num;
+        screen.value += value;
+      }
     });
+  });
 
-    equal.addEventListener('click', function(e){
-        if(screen.value === ''){
-            screen.value = "";
-        }else{
-            let answer = eval(screen.value);
-            screen.value = answer;
-        }
-    })
+  equal.addEventListener("click", function (e) {
+    if (screen.value === "") {
+      screen.value = "";
+    } else {
+      let answer = eval(screen.value);
+      screen.value = answer;
+    }
+  });
 
-    clear.addEventListener('click', function(e){
-        screen.value = "";
-    })
-
+  clear.addEventListener("click", function (e) {
+    screen.value = "";
+  });
 })();
